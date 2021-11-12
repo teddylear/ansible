11ef1e05552fa04835ce963dae25cccaee8e6307
teddylear <teddylear@gmail.com>
2021-11-12 15:02:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ansible
+
+Attempt to automate setting up dependencies and install dotfiles on new machine
+
+Currently [dotfiles repo is here](https://github.com/teddylear/shell_config)
---
+++ install.sh
@@ -0,0 +1,19 @@
+# /bin/bin
+
+# This is only for mac rn, will update later to actually use on ubuntu
+if ! command -v brew &> /dev/null; then
+    echo "brew could not be found, installing"
+    xcode-select --install
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+    exit
+else
+    echo "brew already installed"
+fi
+
+if ! command -v ansible &> /dev/null; then
+    echo "ansible could not be found, installing"
+    brew install ansible
+else
+    echo "ansible already installed"
+fi
+
